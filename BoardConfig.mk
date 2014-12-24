2171dfde718a8c78262ed8877304211b4c6e3d33
Bjorn Hutmacher <bjorn.hutmacher@gmail.com>
2014-12-24 21:59:02 +0100

    Clean up the TWRP stuff a bit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,14 +114,6 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RBG_565"
 TW_SCREEN_BLANK_ON_BOOT := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_NO_USB_STORAGE := false
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-# The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
-#TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
-#TW_MAX_BRIGHTNESS := 255
-#TW_NO_SCREEN_TIMEOUT := true
 
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
